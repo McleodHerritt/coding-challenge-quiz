193649d5f265f6487d8988ba7fb44f93997b45d5
McleodHerritt <nmcleod2021@gmail.com>
2023-08-20 18:11:19 -0300

    made the timer countdown when the quiz starts

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 var startButton = document.querySelector("#startButton");
 var splashScreen = document.getElementById("splashScreen");
 var quizScreen = document.getElementById("quiz");
-var timerInterval = document.querySelector("#timer");
+var timer = document.querySelector("#timer");
 var quizQuestions = document.querySelector("#quiz-questions");
 var question = document.querySelector("#question");
 var highscore = document.querySelector("#highscore");
+var secondsLeft = 60;
+
 startButton.addEventListener("click", startQuiz);
 
 //gets buttons from html for quiz answers
@@ -23,12 +25,7 @@ var quizData = [
 
   {
     question: "What does this equation mean ? a != t .",
-    options: [
-      "A is assinged t",
-      "A and t are equal",
-      "A is not equal to t",
-      "T is add to a",
-    ],
+    options: ["A is assinged t", "A and t are equal", "T is add to a"],
     correctAnswer: "A is not equal to t",
   },
 ];
@@ -45,44 +42,21 @@ function loadQuestion(index) {
   button_2.textContent = currentQuestion.options[1];
   button_3.textContent = currentQuestion.options[2];
   button_4.textContent = currentQuestion.options[3];
-
-  optionsElement.innerHTML = "";
-  currentQuestion.options.forEach((option) => {
-    const optionButton = document.createElement("button");
-    optionButton.textContent = option;
-    optionButton.addEventListener("click", () =>
-      checkAnswer(option, currentQuestion.correctAnswer)
-    );
-    optionsElement.appendChild(optionButton);
-  });
 }
 // chooses if the answer is correct or if the answer is wrong and what to do following
-function checkAnswer(selectedOption, correctAnswer) {
-  if (selectedOption === correctAnswer) {
-    win++;
-  } else {
-    timerInterval(1000);
-  }
+function checkAnswer() {}
+function startTimer() {
+  // Sets interval in variable
+  var timerInterval = setInterval(updateTimer, 1000);
 }
 
-var secondsLeft = 60;
-
-function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left.";
-
-    if (secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
-  }, 1000);
+function updateTimer() {
+  secondsLeft--;
+  timer.textContent = "Time: " + secondsLeft;
 }
 
 function startQuiz() {
   splashScreen.style.display = "none";
   quizScreen.style.display = "block";
+  startTimer();
 }
